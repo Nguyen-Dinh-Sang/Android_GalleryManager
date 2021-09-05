106fc4d299f2f9aa63a31670a18378a85330c35e
SSG\sangnd <sangnd@ssg.vn>
2021-09-05 17:43:22 +0700

    GalleryManager-SangND: add splash screen

---
+++ app/src/main/java/com/example/android_gallerymanager/SplashScreenActivity.java
@@ -0,0 +1,90 @@
+package com.example.android_gallerymanager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        handleSplashScreen();
+    }
+
+    private void start() {
+        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void handleSplashScreen() {
+        if (checkPermission()) {
+            start();
+        }
+    }
+
+    private boolean checkPermission() {
+        return isWriteStoragePermissionGranted() && isReadStoragePermissionGranted();
+    }
+
+    // From api 23 need to ask for permission
+    private boolean isReadStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        } else {
+            // Below api 23 was requested in Manifest
+            return true;
+        }
+    }
+
+    // From api 23 need to ask for permission
+    private boolean isWriteStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+                return false;
+            }
+        } else {
+            // Below api 23 was requested in Manifest
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                   handleSplashScreen();
+                } else {
+                    isReadStoragePermissionGranted();
+                }
+                break;
+            case 2:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                   handleSplashScreen();
+                } else {
+                    isWriteStoragePermissionGranted();
+                }
+                break;
+        }
+    }
+}
